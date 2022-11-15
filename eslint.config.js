e2862569df0970fbe97a9cbebfc3f15e2e739f5f
A1doranM <vaselisk1997@gmail.com>
2022-11-15 13:05:09 +0200

    feat: created skeleton and configured eslint

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    env: {
+      browser: true,
+      es2022: true
+    },
+    extends: ["airbnb", "prettier", "plugin:node/recommended"],
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    plugins: ["prettier"],
+    rules: {}
+  }
+];
