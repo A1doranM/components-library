251ac21fc35e0e8265202ec5c22f887f31643131
A1doranM <vaselisk1997@gmail.com>
2022-11-17 20:27:18 +0200

    fix: Updated all package.json libraries

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,37 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import postcss from "rollup-plugin-postcss";
-import dts from "rollup-plugin-dts";
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const postcss = require("rollup-plugin-postcss");
+const dts = require("rollup-plugin-dts");
 
 const packageJson = require("./package.json");
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
-    ],
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.(css|less|scss)$/],
-  },
+module.exports = [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: "dist/cjs/index.js",
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: "dist/esm/index.js",
+                format: "esm",
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+            typescript({tsconfig: "./tsconfig.json"}),
+            postcss(),
+        ],
+    },
+    {
+        input: "dist/esm/types/index.d.ts",
+        output: [{file: "dist/index.d.ts", format: "esm"}],
+        plugins: [dts.default()],
+        external: [/\.(css|less|scss)$/],
+    },
 ];
