f1544989e81495389de2f9b6e44daa843baa5964
AndriyRubanskui <rubansku97@gmail.com>
2022-12-01 15:33:22 +0200

    fix: resolved conflicts

--- src/components/buttons/RadioButton/tests/RadioButton.test.tsx
+++ src/components/buttons/RadioButton/tests/RadioButton.test.tsx
@@ -5,6 +5,13 @@ import RadioButton from "../RadioButton";
 
 describe("RadioButton", () => {
   test("Renders the RadioButton component", () => {
-    render(<RadioButton name="test" value="test" checked={true} onClick={() => console.log("clicked")}/>);
+    render(
+      <RadioButton
+        name="test"
+        value="test"
+        checked={true}
+        onClick={() => console.log("clicked")}
+      />
+    );
   });
 });
