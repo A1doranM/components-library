c9d6262d3675c3722381aba1099a3028195b909b
A1doranM <vaselisk1997@gmail.com>
2023-02-17 14:32:00 +0200

    fix: Fixed Tootlip behavior when parent element scrolling horizontally

--- dist/components/Tooltip/Tooltip.d.ts
+++ dist/components/Tooltip/Tooltip.d.ts
@@ -1,4 +1,4 @@
 import { TooltipInterface } from "./iTooltip";
 import "./tooltip.scss";
-declare const Tooltip: ({ children, content, show, wrapperClassName, contentClassName }: TooltipInterface) => JSX.Element;
+declare const Tooltip: ({ children, content, show, wrapperClassName, contentClassName, positionRegardingToElementId }: TooltipInterface) => JSX.Element;
 export default Tooltip;
--- dist/components/Tooltip/iTooltip.d.ts
+++ dist/components/Tooltip/iTooltip.d.ts
@@ -4,4 +4,5 @@ export interface TooltipInterface {
     show?: boolean;
     wrapperClassName?: string;
     contentClassName?: string;
+    positionRegardingToElementId?: string;
 }
