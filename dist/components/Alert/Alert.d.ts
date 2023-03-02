8cfa66f768abe6609d9f2ef91ae91bdd20b9ebb4
A1doranM <vaselisk1997@gmail.com>
2023-03-02 11:55:57 +0200

    fix: Changed Alert props

--- dist/components/Alert/Alert.d.ts
+++ dist/components/Alert/Alert.d.ts
@@ -1,12 +1,12 @@
 import React from "react";
 import "./alert.scss";
 export interface AlertInterface {
-    text: string;
     type: "success" | "error" | "warning";
+    children?: any;
     withIcon?: boolean;
     closable?: boolean;
     className?: string;
     onClose?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
-declare const Alert: ({ text, type, withIcon, closable, className, onClose }: AlertInterface) => JSX.Element;
+declare const Alert: ({ children, type, withIcon, closable, className, onClose }: AlertInterface) => JSX.Element;
 export default Alert;
